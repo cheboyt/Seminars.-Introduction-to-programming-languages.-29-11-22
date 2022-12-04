78d33f878280d508c54508ac4cf4c7408706b587
cheboyt <cheboyt@gmail.com>
2022-12-04 22:48:57 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите вотрое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 == num2 * num2)
+{
+    Console.WriteLine("Да");
+}
+else{
+    Console.WriteLine("Нет");
+}
