3b221496dc006619c1c8a9482157035265186957
cheboyt <cheboyt@gmail.com>
2022-12-05 21:48:16 +0300

    homework_lesson_1

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,5 +1,35 @@
 ﻿Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+/*
+switch (num){
+    case 1:
+        Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Вторник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье");
+        break;
+    default:
+        Console.WriteLine("Нет таких значений");
+        break;
+}
+*/
+
+
 if(num == 1)
 {
     Console.WriteLine("Понедельник");
@@ -30,5 +60,5 @@ else if (num == 7)
 }
 else
 {
-    Console.WriteLine("Ошибка");
+    Console.WriteLine("Нет таких значений");
 }
